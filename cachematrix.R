c55c372355ff936c9a53765d471de473f7749830
mackrell <dmackrell@me.com>
2020-07-22 20:04:27 +0800

    Cleaned comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
-# These two functions together will return the inverse of a matrix. Before returning the inverse, it will first check to see if the inverse has already been calculated, and, if so, return the cached version. If it hasn't previously been calculated, it will cache the inverse.
+# These two functions together will return the inverse of a matrix. 
+# Before returning the inverse, it will first check to see if the inverse has already been calculated, 
+# and, if so, return the cached version. If it hasn't previously been calculated, it will cache the inverse.
 
 
-## Write a short comment describing this function
-#This function creates a matrix object that can cache its inverse. It returns a list object, which contains a set of four functions: set, which sets the matrix, get, which gest the matrix, setsolve, which sets the inverse of the matrix (calculated by the cacheSolve function), and getsolve, which gets the solved matrix (the inverse matrix). 
+# This function creates a matrix object that can cache its inverse. It returns a list object, 
+# which contains a set of four functions: set, which sets the matrix, get, which gest the matrix, 
+# setsolve, which sets the inverse of the matrix (calculated by the cacheSolve function), and getsolve, 
+# which gets the solved matrix (the inverse matrix). 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -19,10 +21,8 @@ makeCacheMatrix <- function(x = matrix()) {
          getsolve = getsolve)
 }
 
-
-
-## Write a short comment describing this function
-# Ths functions calculates the inverse of the matrix created by makeCacheMatrix, using the Solve function. It first checks to see if the inverse has already been cached, and if so, returns the cached version instead.
+# This functions calculates the inverse of the matrix created by makeCacheMatrix, using the Solve function. 
+# It first checks to see if the inverse has already been cached, and if so, returns the cached version instead.
 cacheSolve <- function(x, ...) {
     # This function requires an object that is returned by makeVector in order to retrieve the mean
     # from the cached value that is stored in the makeVector object's environment
